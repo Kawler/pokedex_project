dac57eb17a1c5896c3c101dd0a007213cf453273
ArtemL <failedjock@gmail.com>
2024-05-12 23:48:38 +0300

    alert+removed night theme

--- app/src/main/java/com/artemla/pokedex/ui/activities/MainActivity.kt
+++ app/src/main/java/com/artemla/pokedex/ui/activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.artemla.pokedex.ui.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +16,7 @@ import com.artemla.pokedex.R
 import com.artemla.pokedex.databinding.ActivityMainBinding
 import com.artemla.pokedex.domain.utils.PreferencesUtils
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,18 +28,38 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         val preferences = PreferencesUtils.getInstance(context = applicationContext)
         viewModel.fetchDataFromApi()
-        startIntro(preferences.isFirstLaunch())
+        val firstLaunch = preferences.isFirstLaunch()
+        startIntro(firstLaunch)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        alertView(
+            "Currently favourite feature or database are not implemented. The heart button is just a decoration",
+            this,
+            firstLaunch
+        )
         setNavbar()
+    }
+
+    private fun alertView(message: String, context: Context, firstLaunch: Boolean) {
+        if (firstLaunch) {
+            val builder = AlertDialog.Builder(ContextThemeWrapper(context, R.style.Theme_Pokedex))
+            builder.setTitle("Small warning")
+                .setMessage(message)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setCancelable(true) // Prevent the dialog from being dismissed by tapping outside of it
+
+            val alertDialog = builder.create()
+            alertDialog.show()
+        }
     }
 
     private fun setNavbar() {
         val navView: BottomNavigationView = binding.navView
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+            supportFragmentManager.findFragmentById(com.artemla.pokedex.R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -50,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         navView.itemIconTintList = null
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.navigation_home || destination.id == R.id.navigation_pokemon) {
+            if (destination.id == com.artemla.pokedex.R.id.navigation_home || destination.id == com.artemla.pokedex.R.id.navigation_pokemon) {
                 binding.mainActionbar.visibility = View.GONE
             } else {
                 binding.mainActionbar.visibility = View.VISIBLE
